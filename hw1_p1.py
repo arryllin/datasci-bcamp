805167aaefb662c1c82a9e1acccb0a948cf355ae
arryllin <d4rryl.2016@gmail.com>
2024-03-02 14:07:43 -0500

    hw1_p1

--- hw1_p1.py
+++ hw1_p1.py
@@ -1,4 +1,7 @@
-def main():
-    print("Testing")
-
-main()
+def funcCountVowels(varWord):
+    varCount = 0
+    for varLetter in varWord:
+        if ((varLetter=="a") or (varLetter=="e") or (varLetter=="i") or (varLetter=="o") or (varLetter=="u") or 
+            (varLetter=="A") or (varLetter=="E") or (varLetter=="I") or (varLetter=="O") or (varLetter=="U")):
+            varCount += 1
+    return varCount
